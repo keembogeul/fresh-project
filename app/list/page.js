77036ed837ec8b708d9115facbbf1d31f1629e55
keembogeul <318qhtmf@naver.com>
2023-05-17 23:42:38 +0900

    장바구니 +, - 버튼 만들기 (useState 사용)

--- app/list/page.js
+++ app/list/page.js
@@ -1,8 +1,22 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
 
 export default function List() {
+  /* 
+  - 변수: 데이터 일시적으로 저장 가능 == state
+  - state 장점: state 변경되면 state를 사용하는 html 부분도 자동으로 재렌더링
+  */
   let products = ["Tomatoes", "Pasta", "Coconut"];
 
+  // [ 변수 이름, amount 변경 시 사용할 이벤트 ]
+  let [amount, changeAmount] = useState([0, 0, 0]);
+
+  /* 
+  
+  #(정보) state 변경 함수 사용 시 new state == old state일 경우 변경 X
+
+  */
   return (
     <div>
       <h4 className="title">상품목록</h4>
@@ -11,6 +25,25 @@ export default function List() {
           <div className="food" key={i}>
             <img src={`/food${i}.png`} className="food-img" />
             <h4>{a} $40</h4>
+            <span> {amount[i]} </span>
+            <button
+              onClick={() => {
+                let copy = [...amount];
+                copy[i]++;
+                changeAmount(copy);
+              }}
+            >
+              +
+            </button>
+            <button
+              onClick={() => {
+                let copy = [...amount];
+                copy[i]--;
+                changeAmount(copy);
+              }}
+            >
+              -
+            </button>
           </div>
         );
       })}
